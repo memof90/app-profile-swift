b9da54df833283adae9fd253b06ec2a689446977
memo figueredo <memof80@gmail.com>
2020-12-15 16:38:08 -0500

    Initial setup

---
+++ dev-profile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dev-profile
+//
+//  Created by Memo Figueredo on 15/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
